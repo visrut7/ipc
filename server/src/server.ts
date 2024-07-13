150cda822058032bb12ee8fc58f657e74d597b69
visrut <visrut@incubyte.co>
2024-07-13 13:30:07 +0530

    send image data in the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,32 @@
 import * as http from "http";
+import * as fs from "fs/promises";
+import formidable from "formidable";
 
-const server = http.createServer((req, res) => {
-  res.write("Helllo world");
-  res.end();
+const server = http.createServer(async (req, res) => {
+  console.log(req.method, req.url);
+
+  if (req.method === "GET" && req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    const html = await fs.readFile("public/index.html", { encoding: "utf-8" });
+    res.end(html);
+    return;
+  }
+
+  if (req.method === "POST" && req.url === "/image") {
+    const form = formidable({ multiples: false, uploadDir: "uploads" });
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("Error parsing the form");
+        console.error(err);
+        return;
+      }
+
+      const imageData = fields.image;
+      console.log(imageData);
+    });
+  }
 });
 
 server.listen(3000, () => {
